53ee8f6ad2fbce5e8b993c19da611770620c454c
gastonfr24 <gastonfr24@gmail.com>
2025-11-25 11:44:02 -0300

    fix(backend): fix CORS config to use env instead of config [T-001]

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -154,8 +154,8 @@ REST_FRAMEWORK = {
 
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = config(
-    "CORS_ALLOWED_ORIGINS", default="http://localhost:3000,http://127.0.0.1:3000"
-).split(",")
+CORS_ALLOWED_ORIGINS = env.list(
+    "CORS_ALLOWED_ORIGINS", default=["http://localhost:3000", "http://127.0.0.1:3000"]
+)
 
 CORS_ALLOW_CREDENTIALS = True
